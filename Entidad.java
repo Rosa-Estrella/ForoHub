c0ef997daa6250725dee1e180a9cebfa7517c6ec
Rosa-Estrella <calderonrosaestrella07@gmail.com>
2025-08-16 18:56:34 -0400

    Create Entidad.java

---
+++ Entidad.java
@@ -0,0 +1,24 @@
+package com.alura.foro.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "topicos")
+@Getter @Setter @NoArgsConstructor
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String mensaje;
+
+    private String autor;
+}
